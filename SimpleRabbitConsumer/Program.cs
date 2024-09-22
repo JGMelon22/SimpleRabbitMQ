efaed267004d3335836eee00903c8d6e977c4cda
JGMelon22 <jgvasconcellos22@gmail.com>
2024-09-22 15:25:35 -0300

    feat(consumer): develop queue consumer

--- SimpleRabbitConsumer/Program.cs
+++ SimpleRabbitConsumer/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "172.17.0.3" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($" [X] Received Message: {message}");
+};
+
+channel.BasicConsume(queue: "orders",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine($" Press [enter] to exit.");
+Console.ReadKey();
